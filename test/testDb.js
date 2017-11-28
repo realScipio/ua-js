7bb57ece791fcdb5fb938130107797b06a3e889d
stoodkev <32375533+stoodkev@users.noreply.github.com>
2017-11-28 14:26:50 +0800

    Show output

--- test/testDb.js
+++ test/testDb.js
@@ -11,6 +11,14 @@ MongoClient.connect("mongodb://localhost:27017/uaJS",function(err, db) {
 			{"id":4,"name":"D","followers":["B","E"],"following_count":2},
 			{"id":5,"name":"E","followers":["D","F"],"following_count":3},
 			{"id":6,"name":"F","followers":["E"],"following_count":2}]
-			).then(function(result){db.close();});
+			).then(function(result){
+                showDB(db);});
 	}
 });
+
+async function showDB(db){
+
+    const show=await db.collection("testDB").find({});
+    console.log(await show.toArray());
+    db.close();
+}
